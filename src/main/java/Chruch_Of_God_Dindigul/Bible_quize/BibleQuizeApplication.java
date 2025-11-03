277f13e080bed880dc51a2456ddffce67af0ee42
DevottaAbraham <abdevotta@gmail.com>
2025-11-03 19:53:55 +0530

    The Fiels Are Tracked And Added

--- src/main/java/Chruch_Of_God_Dindigul/Bible_quize/BibleQuizeApplication.java
+++ src/main/java/Chruch_Of_God_Dindigul/Bible_quize/BibleQuizeApplication.java
@@ -2,11 +2,14 @@ package Chruch_Of_God_Dindigul.Bible_quize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 // Add this annotation to explicitly tell Spring where to find your repository interfaces.
 @EnableJpaRepositories(basePackages = "Chruch_Of_God_Dindigul.Bible_quize.repository")
+// Add this annotation to explicitly tell Spring where to find your @Entity classes.
+@EntityScan(basePackages = "Chruch_Of_God_Dindigul.Bible_quize.model")
 public class BibleQuizeApplication {
 
 	public static void main(String[] args) {
