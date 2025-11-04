b73950581cdf4e021ada76e248a553fa4dd537bd
DevottaAbraham <abdevotta@gmail.com>
2025-11-04 19:04:22 +0530

    Tracked And Added

---
+++ src/main/java/Chruch_Of_God_Dindigul/Bible_quize/config/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package Chruch_Of_God_Dindigul.Bible_quize.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${DB_USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+    }
+}
